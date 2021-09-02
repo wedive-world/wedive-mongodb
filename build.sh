f85fb15196eb6aaa17f729201f50fef7c3e3500e
Mark Yun <only.hyeok@gmail.com>
2021-09-02 22:25:33 +0900

    fix

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker pull mongo &&\
-docker stop mongodb || true &&\
-docker rm mongodb || true &&\
+docker stop mongodb || true &&
+docker rm mongodb || true &&
 docker run --name mongodb -v ~/data:/data/db -d -p 27017:27017 mongo
