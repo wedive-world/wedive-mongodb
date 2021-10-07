99fb8c8497e1c758cc664c77be5ca064cdb03935
Mark Yun <only.hyeok@gmail.com>
2021-10-07 23:19:25 +0900

    changed deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-cp /wedive-secret/mongo-express-secret.env ./ &&
-cp /wedive-secret/db-backup-config.env ./ &&
-cp /wedive-secret/mongo-config.env ./ &&
+cp /wedive-secret/mongo-express-secret.env ./wedive-secret/ &&
+cp /wedive-secret/db-backup-config.env ./wedive-secret/ &&
+cp /wedive-secret/mongo-config.env ./wedive-secret/ &&
 openssl rand -base64 741 > mongo-keyfile &&
 docker-compose build &&
 docker-compose up -d &&
