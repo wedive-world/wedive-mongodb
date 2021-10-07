bd181b8e696d51bc326d88d80ef53a3ccf10c511
Mark Yun <only.hyeok@gmail.com>
2021-10-07 23:32:01 +0900

    added chmod to keyfile

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
-cp /wedive-secret/mongo-express-secret.env ./wedive-secret/ &&
-cp /wedive-secret/db-backup-config.env ./wedive-secret/ &&
-cp /wedive-secret/mongo-config.env ./wedive-secret/ &&
+cp /wedive-secret/mongo-express-secret.env ./wedive-secret/ || true &&
+cp /wedive-secret/db-backup-config.env ./wedive-secret/  || true &&
+cp /wedive-secret/mongo-config.env ./wedive-secret/  || true &&
 openssl rand -base64 741 > mongo-keyfile &&
+chmod 400 mongo-keyfile &&
 docker-compose build &&
 docker-compose up -d &&
 docker image prune -a -f
