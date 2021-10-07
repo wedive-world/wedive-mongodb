9a9ff5eb2940656cdbae753d79c957acd90c840f
Mark Yun <only.hyeok@gmail.com>
2021-10-07 23:12:25 +0900

    added scretKey

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 cp /wedive-secret/mongo-express-secret.env ./ &&
 cp /wedive-secret/db-backup-config.env ./ &&
 cp /wedive-secret/mongo-config.env ./ &&
-docker-compose build && \
+openssl rand -base64 741 > mongo-keyfile &&
+docker-compose build &&
 docker-compose up -d &&
 docker image prune -a -f
