33f04f20d3406d19044c612f14d97e66b746d5de
Mark Yun <only.hyeok@gmail.com>
2021-10-08 00:08:50 +0900

    fixed issue

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 cp /wedive-secret/mongo-express-secret.env ./wedive-secret/ || true &&
 cp /wedive-secret/db-backup-config.env ./wedive-secret/  || true &&
 cp /wedive-secret/mongo-config.env ./wedive-secret/  || true &&
-openssl rand -base64 741 > mongo-keyfile &&
-chmod 600 mongo-keyfile &&
 docker-compose build &&
 docker-compose up -d &&
 docker image prune -a -f
