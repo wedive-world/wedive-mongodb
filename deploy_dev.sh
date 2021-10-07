ede2377fe51688f147a8c0b77d4bae49ed308779
Mark Yun <mark.yun89@gmail.com>
2021-10-07 16:39:31 +0900

    added replica config

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,5 +1,6 @@
 cp ../wedive-secret/mongo-express-secret.env ./ &&
 cp ../wedive-secret/db-backup-config.env ./ &&
+cp ../wedive-secret/mongo-config.env ./ &&
 docker-compose build && \
 docker-compose up -d &&
 docker image prune -a -f
---
+++ replica/replicaSet.js
@@ -0,0 +1,12 @@
+config = {
+    _id: "replication",
+    members: [
+      { _id: 0, host: "mongo:27017" },
+      { _id: 1, host: "mongo-replica-1:27017" },
+      { _id: 2, host: "mongo-replica-2:27017" }
+    ],
+  }
+  
+  rs.initiate(config)
+  
+  rs.conf()
