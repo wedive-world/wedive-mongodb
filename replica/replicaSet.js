51ef602846ec3738461920d330e2b30b2389d061
Mark Yun <only.hyeok@gmail.com>
2021-10-09 13:44:31 +0900

    removed replica set

--- replica/replicaSet.js
+++ replica/replicaSet.js
@@ -2,8 +2,8 @@ config = {
   _id: "replication",
   members: [
     { _id: 0, host: "mongo:27017" },
-    { _id: 1, host: "mongo-replica-1:27017" },
-    { _id: 2, host: "mongo-replica-2:27017" }
+    // { _id: 1, host: "mongo-replica-1:27017" },
+    // { _id: 2, host: "mongo-replica-2:27017" }
   ],
 }
 
